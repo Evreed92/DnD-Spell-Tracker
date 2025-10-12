6b52ff5d4609fb760cdc2b84db9d8907416c7fc9
Evan Reed <evanreed@macbookair.attlocal.net>
2025-10-11 21:11:55 -0500

    Reorginize default files to template folder

--- Program.cs
+++ Program.cs
@@ -4,18 +4,6 @@ using myTuiProj.Views;
 using Terminal.Gui;
 
 Application.Init();
-var top = Application.Top;
-
-top.Add(new Label("Press Q to Quit") { X = 1, Y = 1 });
-
-top.KeyPress += (args) =>
-{
-    if (args.KeyEvent.Key == Key.Q) // Change to whatever key you want
-    {
-        Application.RequestStop();
-        args.Handled = true;
-    }
-};
 
 try
 {
