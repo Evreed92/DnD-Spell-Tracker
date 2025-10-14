4806b2a7826c986b04f9f24a0f92c00a58dbbe45
Evan Reed <evanreed@macbookair.attlocal.net>
2025-10-13 22:10:25 -0500

    Update to the character class. Rethinking all data structures for future plans

--- Domain/Models/Character.cs
+++ Domain/Models/Character.cs
@@ -8,13 +8,16 @@ namespace myTuiProj.Domain.Models
         public string Name { get; set; }
         public string Class { get; set; }
         public int Level { get; set; }
-        //     public string Race { get; set; }
-        //     public string SubClass { get; set; }
-        /* Removed for Debugging
+        public string Race { get; set; } //Immportant for Stat Building
+        public string SubClass { get; set; }
+       //Basic Main Stats
+        public Dictionary<String, int> AbilityScores {get; set;}
+        public Dictionary<String, int> AbilityModifiers {get; set;} //Derived from AbilityScores
+       public int ProficiencyBonus {get; set;}
         //Spell Casting Details
-        public string SpellCastingStat { get; set; }
-        public int SpellSaveDC { get; set; }
-        public int SpellAttackBonus { get; set; }
+        public string SpellCastingStat { get; set; } //From Class
+        public int SpellSaveDC { get; set; } //Derived from statModifier
+        public int SpellAttackBonus { get; set; } //Derived from statModifier
 
         public Dictionary<SpellLevel, int> SpellSlotsMax { get; set; } = new();
     */
